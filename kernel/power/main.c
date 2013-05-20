b2ea9699e751703bf6aa29e2f0493b41641face8
civato <civato@ubuntu.(none)>
2013-05-20 17:56:12 +0200

    Keep support for Samsung CPU power saving mode

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -480,6 +480,9 @@ static ssize_t cpufreq_max_limit_store(struct kobject *kobj,
 			printk(KERN_ERR "%s: Unlock request is ignored\n",
 				__func__);
 	} else { /* Lock request */
+		if (val < 1400000) {
+			val = 1100000;
+
 		if (get_cpufreq_level((unsigned int)val, &cpufreq_level)
 		    == VALID_LEVEL) {
 			if (cpufreq_max_limit_val != -1)
@@ -494,6 +497,7 @@ static ssize_t cpufreq_max_limit_store(struct kobject *kobj,
 		} else /* Invalid lock request --> No action */
 			printk(KERN_ERR "%s: Lock request is invalid\n",
 				__func__);
+		}
 	}
 
 	ret = n;
