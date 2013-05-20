3f734ad75005a0b6e15d4d3ab871ed0cb7187332
civato <civato@ubuntu.(none)>
2013-05-20 17:52:02 +0200

    OC/UC support 50MHz-1900MHz

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -64,7 +64,9 @@ int exynos_verify_speed(struct cpufreq_policy *policy)
 
 unsigned int exynos_getspeed(unsigned int cpu)
 {
-	return clk_get_rate(exynos_info->cpu_clk) / 1000;
+	unsigned int ret = clk_get_rate(exynos_info->cpu_clk) / 1000;
+	if(ret == 1704000) ret = 1700000;
+	return ret;
 }
 
 static unsigned int exynos_get_safe_armvolt(unsigned int old_index, unsigned int new_index)
@@ -729,7 +731,13 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 		cpumask_setall(policy->cpus);
 	}
 
-	return cpufreq_frequency_table_cpuinfo(policy, exynos_info->freq_table);
+	cpufreq_frequency_table_cpuinfo(policy, exynos_info->freq_table);
+
+	/* Safe default startup limits */
+	policy->max = 1600000;
+	policy->min = 200000;
+
+	return 0;
 }
 
 static int exynos_cpufreq_reboot_notifier_call(struct notifier_block *this,
